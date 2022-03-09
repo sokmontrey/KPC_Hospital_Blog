6a9e2e75e46165502c21de066ac205dc9840c5e5
SokmontreyTDSB <100730097+SokmontreyTDSB@users.noreply.github.com>
2022-03-09 12:08:45 -0500

    Update DatabaseController.js

--- server/Controllers/DatabaseController.js
+++ server/Controllers/DatabaseController.js
@@ -4,10 +4,24 @@ import PostModel from '../Models/PostModel.js';
 const mongoDB_URI = "mongodb+srv://admin:Admin1234@cluster0.b1qmj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(mongoDB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-export async function create(data){
-	await PostModel.create(data).then(() => {
-		console.log("Post created");
-	}).catch(err => {
-		console.log(err);
-	});
+//callback param have to be (err, result)
+export async function create(data, callback){
+	await PostModel.create(data, callback);
 }
+
+export async function update(id, newData, callback){
+	await PostModel.findOneAndUpdate({_id: id}, newData, callback);
+}
+
+export async function remove(id, callback){
+	await PostModel.deleteOne({_id: id}, callback);
+}
+
+export async function getOne(id, callback){
+	await PostModel.fineOne({_id: id}, callback);
+}
+
+export async function getMany(index, limit, callback){
+	await PostModel.findManyu
+}
+
