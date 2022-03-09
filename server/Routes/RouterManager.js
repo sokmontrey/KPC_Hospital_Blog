7e684f2a189ce059ea2097f0eba8acfd33bbcb53
Sokmontrey <sythatsokmontrey@gmail.com>
2022-03-09 17:51:13 -0500

    direct route to post and admin

--- server/Routes/RouterManager.js
+++ server/Routes/RouterManager.js
@@ -1,7 +1,25 @@
+import { 
+	getHomePost,
+	viewPost,
+} from './PostRoute.js';
+
+import {
+	verifyAdmin,
+	getAll,
+	createPost,
+	updatePost,
+	removePost,
+} from './AdminRoute.js';
+
 export default function RouterManager(router){
-	router.get('/', function(req, res){
-		console.log('WORKING');
-	});
+	router.get('/', getHomePost);
+	router.get('/view/:id', viewPost);
+
+	router.post('/admin', verifyAdmin);
+	router.post('/admin/list', getAll);
+	router.post('/admin/create', createPost);
+	router.post('/admin/update', updatePost);
+	router.post('/admin/remove', removePost);
 	return router;
 };
 
