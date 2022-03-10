bd48b649063f22e83d45bea77b083f511f5b3bba
Sokmontrey <sythatsokmontrey@gmail.com>
2022-03-09 19:55:07 -0500

    add _sendRes function"

--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -3,6 +3,14 @@ import {
 	getOne,
 } from '../Controllers/DatabaseController.js';
 
+function _sendRes(res, isSuccess, message, payload){
+	res.status(isSuccess ? 200 : 501).send({ 
+		success: isSuccess,
+		message: message,
+		payload: payload
+	});
+}
+
 export async function getHomePost(req, res){
 	const body = req.body;
 	const index = body.index || 0;
@@ -11,14 +19,14 @@ export async function getHomePost(req, res){
 	var skip = index * limit;
 
 	await getMany(skip, limit, (err, results)=>{
-		if(err) res.status(500).send({error: err});
-		else res.status(200).send({ postList: results });
+		if(err) _sendRes(res, false, err, null);
+		else _sendRes(res, true, 'Success', results);
 	});
 }
 export async function viewPost(req, res){
 	const id = req.params.id;
 	await getOne(id, (err, result)=>{
-		if(err) res.status(500).send({error: err});
-		else res.status(200).send({ post: result });
+		if(err) _sendRes(res, false, err, null);
+		else _sendRes(res, true, 'Success', result);
 	});
 }
